5f12c957981929d440841252e1e9eaf952e98e86
wuhuaa1 <916607025@qq.com>
2022-07-22 16:59:11 +0800

    取消redis接口白名单 knife4j接口文档格式优化

--- ruoyi-news/src/main/java/com/ruoyi/news/Controller/RedisController02.java
+++ ruoyi-news/src/main/java/com/ruoyi/news/Controller/RedisController02.java
@@ -1,21 +1,24 @@
 package com.ruoyi.news.Controller;
 
 
+import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 @RestController
-@Api(value = "redis控制器")
+@Api(tags = "redis控制器")
 @RequestMapping("/redis")
 public class RedisController02 {
 
@@ -23,15 +26,14 @@ public class RedisController02 {
     @Autowired
     RedisTemplate redisTemplate;
 
-
     /**\
      * 添加redis键
      * @param value
      * @return
      */
     @PostMapping("/add")
-    @ApiOperation(value = "添加redis数据")
-    public AjaxResult add(@ApiParam  String value)
+    @ApiOperation(value = "添加redis数据带时间",notes = "添加redis数据带时间")
+    public AjaxResult add(String value)
     {
         //获取当前时间
         LocalTime time = LocalTime.now();
@@ -41,38 +43,38 @@ public class RedisController02 {
         String getValue =value+"time:"+time.format(formatter);
 
         redisTemplate.opsForValue().set("addRedis"+value,getValue,3, TimeUnit.MINUTES);
-        return AjaxResult.success("添加"+getValue+"成功");
+        return AjaxResult.success("添加redis"+getValue+"成功");
 
     }
+
     /**\
      * 添加redis键
-     * @param value
+     * @param key
      * @return
      */
-    @PostMapping("/add2")
-    @ApiOperation(value = "添加redis数据不带时间")
-    public AjaxResult add2(@ApiParam  String value)
+    @GetMapping("/add2")
+    @ApiOperation(value = "添加redis2",notes = "添加redis2")
+    public AjaxResult add2( String key)
     {
-        redisTemplate.opsForValue().set("addRedis"+value,value,3, TimeUnit.MINUTES);
-        return AjaxResult.success("添加"+value+"成功");
+        redisTemplate.opsForValue().set("addRedis"+key,key,3, TimeUnit.MINUTES);
+        return AjaxResult.success("添加redis2"+key+"成功");
 
     }
 
     /**\
      * 验证redis key是否存在
-     * @param value
-     * @return
+     key     * @return
      */
-    @PostMapping("/select")
-    @ApiOperation(value = "验证redis key是否存在")
-    public AjaxResult select(@ApiParam  String value)
+    @GetMapping(value = "/select")
+    @ApiOperation("验证redis key是否存在")
+    public AjaxResult select(@ApiParam(value = "验证rediskey", name = "rediskey") String rediskey)
     {
 
-        String key = "addRedis" + value;
+        String key = "addRedis" + rediskey;
         if (redisTemplate.hasKey(key)) {
-            return AjaxResult.success("key" + value + "存在");
+            return AjaxResult.success("key" + key + "存在");
         } else {
-            return AjaxResult.success("key" + value + "不存在");
+            return AjaxResult.success("key" + key + "不存在");
         }
 
     }
